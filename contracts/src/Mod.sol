bec38f101269b9ac56d6e86519c7b9cc150e98d9
Jimmy Whitwell <jimmy.whitwell@playmint.com>
2022-11-08 11:01:43 +0000

    Linting

--- contracts/src/Mod.sol
+++ contracts/src/Mod.sol
@@ -15,25 +15,25 @@ struct ModData {
 }
 
 enum GameObjectAttr {
-    resonance,     // 0
-    health,        // 1
-    attack,        // 2
-    criticalHit,   // 3
-    agility,       // 4
-    scout,         // 5
-    capacity,      // 6
-    endurance,     // 7
-    harvest,       // 8
-    yieldBonus,    // 9
+    resonance, // 0
+    health, // 1
+    attack, // 2
+    criticalHit, // 3
+    agility, // 4
+    scout, // 5
+    capacity, // 6
+    endurance, // 7
+    harvest, // 8
+    yieldBonus, // 9
     assemblySpeed, // 10
-    modBonus       // 11
+    modBonus // 11
 }
 
 contract Mod is ERC721Enumerable, Ownable {
     uint256 public count;
     mapping(uint256 => ModData) public _tokenData;
 
-    Seeker public  _seekerContract;
+    Seeker public _seekerContract;
 
     // modId to seekerId
     mapping(uint256 => uint256) public _modToSeeker;
@@ -45,23 +45,38 @@ contract Mod is ERC721Enumerable, Ownable {
         _seekerContract = seekerContract;
     }
 
-    function exists(uint256 tokenId) public view returns(bool) {
+    function exists(uint256 tokenId) public view returns (bool) {
         return _exists(tokenId);
     }
 
     function equip(uint256 modId, uint256 seekerId) public {
-        require (_msgSender() == ownerOf(modId), "Mod::equip: sender not owner of mod token");
-        require (_modToSeeker[modId] == 0, "Mod::equip: Mod already equipped");
-        require (_msgSender() == _seekerContract.ownerOf(seekerId), "Mod::equip: sender not owner of seeker");
+        require(
+            _msgSender() == ownerOf(modId),
+            "Mod::equip: sender not owner of mod token"
+        );
+        require(_modToSeeker[modId] == 0, "Mod::equip: Mod already equipped");
+        require(
+            _msgSender() == _seekerContract.ownerOf(seekerId),
+            "Mod::equip: sender not owner of seeker"
+        );
 
         _modToSeeker[modId] = seekerId;
         _seekerToMods[seekerId].push(modId); // Should this be emitting an event instead of recording this on-chain?
     }
 
     function unequip(uint256 modId, uint256 seekerId) public {
-        require (_msgSender() == ownerOf(modId), "Mod::unequip: sender not owner of mod token");
-        require (_modToSeeker[modId] == seekerId, "Mod::unequip: Mod not equipped to seeker");
-        require (_msgSender() == _seekerContract.ownerOf(seekerId), "Mod::unequip: sender not owner of seeker");
+        require(
+            _msgSender() == ownerOf(modId),
+            "Mod::unequip: sender not owner of mod token"
+        );
+        require(
+            _modToSeeker[modId] == seekerId,
+            "Mod::unequip: Mod not equipped to seeker"
+        );
+        require(
+            _msgSender() == _seekerContract.ownerOf(seekerId),
+            "Mod::unequip: sender not owner of seeker"
+        );
 
         _modToSeeker[modId] = 0;
 
@@ -70,19 +85,25 @@ contract Mod is ERC721Enumerable, Ownable {
         uint256 len = _seekerToMods[seekerId].length;
         for (uint i = 0; i < len; i++) {
             if (_seekerToMods[seekerId][i] == modId) {
-                _seekerToMods[seekerId][i] = _seekerToMods[seekerId][len -1];
+                _seekerToMods[seekerId][i] = _seekerToMods[seekerId][len - 1];
                 _seekerToMods[seekerId].pop();
                 break;
             }
         }
     }
 
-    function getSeekerMods(uint256 seekerId) public view returns (uint256[] memory) {
+    function getSeekerMods(
+        uint256 seekerId
+    ) public view returns (uint256[] memory) {
         return _seekerToMods[seekerId];
     }
 
     // mint
-    function mint(address to, GameObjectAttr attr, uint8 value) public returns (uint256 tokenId) {
+    function mint(
+        address to,
+        GameObjectAttr attr,
+        uint8 value
+    ) public returns (uint256 tokenId) {
         count++;
         tokenId = count;
         _tokenData[tokenId] = ModData(tokenId, attr, value);
@@ -90,11 +111,15 @@ contract Mod is ERC721Enumerable, Ownable {
         return tokenId;
     }
 
-    function getData(uint256 tokenId) public view returns (ModData memory data) {
+    function getData(
+        uint256 tokenId
+    ) public view returns (ModData memory data) {
         return _tokenData[tokenId];
     }
 
-    function getModdedSeekerAttrs(uint256 seekerId) public view returns (uint8[12] memory) {
+    function getModdedSeekerAttrs(
+        uint256 seekerId
+    ) public view returns (uint8[12] memory) {
         uint8[12] memory attrs = _seekerContract.getAttrs(seekerId);
         for (uint i = 0; i < _seekerToMods[seekerId].length; i++) {
             uint256 modId = _seekerToMods[seekerId][i];
@@ -104,5 +129,4 @@ contract Mod is ERC721Enumerable, Ownable {
 
         return attrs;
     }
-
 }
--- contracts/src/Seeker.sol
+++ contracts/src/Seeker.sol
@@ -9,13 +9,13 @@ import "encoding/Base64.sol";
 struct SeekerData {
     uint id;
     uint generation;
-    uint8 resonance;   // 0
-    uint8 health;  // 1
-    uint8 attack;  // 2
-    uint8 criticalHit;      // 3
-    uint8 agility;   // 4
-    uint8 scout;  // 5
-    uint8 capacity;    // 6
+    uint8 resonance; // 0
+    uint8 health; // 1
+    uint8 attack; // 2
+    uint8 criticalHit; // 3
+    uint8 agility; // 4
+    uint8 scout; // 5
+    uint8 capacity; // 6
     uint8 endurance; // 7
     uint8 harvest; // 8
     uint8 yieldBonus; // 9
@@ -23,21 +23,34 @@ struct SeekerData {
     uint8 modBonus; // 11
 }
 
+struct Position {
+    uint128 x;
+    uint128 y;
+}
+
 contract Seeker is ERC721Enumerable, Ownable {
     uint256 public _count;
     uint256[16] public _totalMinted; // by generation
     uint256[16] public _maxMintable; // by generation
     mapping(uint256 => uint256) public _attrs;
+    mapping(uint256 => Position) public _position;
 
     constructor() ERC721("Seeker", "Seeker") Ownable() {}
 
-    function exists(uint256 tokenId) public view returns(bool) {
+    function exists(uint256 tokenId) public view returns (bool) {
         return _exists(tokenId);
     }
 
     // mint seeker
-    function mint(address to, uint8 generation, uint8[12] memory attrs) public returns (uint256 tokenId) {
-        require(_totalMinted[generation] < _maxMintable[generation], "tokenId out of range for genesis");
+    function mint(
+        address to,
+        uint8 generation,
+        uint8[12] memory attrs
+    ) public returns (uint256 tokenId) {
+        require(
+            _totalMinted[generation] < _maxMintable[generation],
+            "tokenId out of range for genesis"
+        );
         tokenId = _count + 1;
         _totalMinted[generation]++;
         _attrs[tokenId] = packAttrs(attrs);
@@ -46,23 +59,28 @@ contract Seeker is ERC721Enumerable, Ownable {
         return tokenId;
     }
 
-    function packAttrs(uint8[12] memory attrs) private pure returns(uint256 packed) {
-        return 0
-        | (uint256(attrs[0]) << 0)
-        | (uint256(attrs[1]) << 8)
-        | (uint256(attrs[2]) << 16)
-        | (uint256(attrs[3]) << 24)
-        | (uint256(attrs[4]) << 32)
-        | (uint256(attrs[5]) << 40)
-        | (uint256(attrs[6]) << 48)
-        | (uint256(attrs[7]) << 56)
-        | (uint256(attrs[8]) << 64)
-        | (uint256(attrs[9]) << 72)
-        | (uint256(attrs[10]) << 80)
-        | (uint256(attrs[11]) << 88);
+    function packAttrs(
+        uint8[12] memory attrs
+    ) private pure returns (uint256 packed) {
+        return
+            0 |
+            (uint256(attrs[0]) << 0) |
+            (uint256(attrs[1]) << 8) |
+            (uint256(attrs[2]) << 16) |
+            (uint256(attrs[3]) << 24) |
+            (uint256(attrs[4]) << 32) |
+            (uint256(attrs[5]) << 40) |
+            (uint256(attrs[6]) << 48) |
+            (uint256(attrs[7]) << 56) |
+            (uint256(attrs[8]) << 64) |
+            (uint256(attrs[9]) << 72) |
+            (uint256(attrs[10]) << 80) |
+            (uint256(attrs[11]) << 88);
     }
 
-    function unpackAttrs(uint256 packed) private pure returns(uint8[12] memory attrs) {
+    function unpackAttrs(
+        uint256 packed
+    ) private pure returns (uint8[12] memory attrs) {
         attrs[0] = uint8((packed >> 0) & 0xff);
         attrs[1] = uint8((packed >> 8) & 0xff);
         attrs[2] = uint8((packed >> 16) & 0xff);
@@ -81,30 +99,42 @@ contract Seeker is ERC721Enumerable, Ownable {
         return unpackAttrs(_attrs[tokenId]);
     }
 
-    function getData(uint256 tokenId) public view returns (SeekerData memory data) {
+    function getData(
+        uint256 tokenId
+    ) public view returns (SeekerData memory data) {
         uint8[12] memory attrs = unpackAttrs(_attrs[tokenId]);
-        data.id              = tokenId;
-        data.generation      = 1; // TODO: pull from _data
-        data.resonance       = attrs[0];
-        data.health          = attrs[1];
-        data.attack          = attrs[2];
-        data.criticalHit     = attrs[3];
-        data.agility         = attrs[4];
-        data.scout           = attrs[5];
-        data.capacity        = attrs[6];
-        data.endurance       = attrs[7];
-        data.harvest         = attrs[8];
-        data.yieldBonus      = attrs[9];
-        data.craftingSpeed   = attrs[10];
-        data.modBonus        = attrs[11];
+        data.id = tokenId;
+        data.generation = 1; // TODO: pull from _data
+        data.resonance = attrs[0];
+        data.health = attrs[1];
+        data.attack = attrs[2];
+        data.criticalHit = attrs[3];
+        data.agility = attrs[4];
+        data.scout = attrs[5];
+        data.capacity = attrs[6];
+        data.endurance = attrs[7];
+        data.harvest = attrs[8];
+        data.yieldBonus = attrs[9];
+        data.craftingSpeed = attrs[10];
+        data.modBonus = attrs[11];
 
         return data;
     }
 
-    function setMaxSupply(uint256 generation, uint256 maxMintable) public onlyOwner {
+    function setMaxSupply(
+        uint256 generation,
+        uint256 maxMintable
+    ) public onlyOwner {
         require(_maxMintable[generation] == 0, "supply is immutable");
         _maxMintable[generation] = maxMintable;
     }
 
+    // TODO: Only set by actions contract
+    function setPosition(uint256 seekerId, Position pos) public {
+        _position[seekerId] = pos;
+    }
 
+    function getPosition(uint256 seekerId) public returns (Position memory) {
+        return _position[seekerId];
+    }
 }
